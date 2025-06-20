ba9f182bc0330835c8b5bc7a74145aa9ed9fa359
Emmanuel Badmus <emmanuelabadmus23@gmail.com>
2025-06-19 23:43:11 -0500

    password bar

--- bdaapp/src/SignUpForm/SignUpForm.js
+++ bdaapp/src/SignUpForm/SignUpForm.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SignUpForm.css';
 
 const SignUpForm = () => {
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
   const handleGuest = () => {
     alert('Continue as Guest clicked');
   };
@@ -15,46 +18,69 @@ const SignUpForm = () => {
     alert('Form submitted');
   };
 
+  const calculateStrength = (pass) => {
+    if (!pass) return 'empty';
+    if (pass.length < 6) return 'weak';
+    if (pass.match(/[A-Z]/) && pass.match(/[0-9]/) && pass.length >= 8) return 'strong';
+    return 'medium';
+  };
+
+  const passwordStrength = calculateStrength(password);
+  const confirmStrength = calculateStrength(confirmPassword);
+
   return (
     <div className="page-container">
       <h1 className="header1">Welcome!</h1>
 
-<div className="signup-container">
+      <div className="signup-container">
+        <div className="right-column">
+          <h2 className="header2">Sign Up</h2>
+          <form onSubmit={handleSubmit}>
 
-  {/* Left Column */}
-  <div className="right-column">
-    <h2 className="header2">Sign Up</h2>
-    <form onSubmit={handleSubmit}>
-      <div className="inputs">
-        <input className="input" placeholder="Username" />
+            <div className="inputs">
+              <input className="input" placeholder="Username" />
+            </div>
+
+            <div className="inputs">
+              <input className="input" type="email" placeholder="Email" />
+            </div>
+
+            {/* Password Field */}
+            <div className="inputs">
+              <input
+                className={`input ${passwordStrength}`}
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <div className={`strength-bar ${passwordStrength}`}></div>
+            </div>
+
+            {/* Confirm Password Field */}
+            <div className="inputs">
+              <input
+                className={`input ${confirmStrength}`}
+                type="password"
+                placeholder="Confirm Password"
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+              <div className={`strength-bar ${confirmStrength}`}></div>
+            </div>
+
+            <br />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+
+        <div className="or-separator">
+          <span>OR</span>
+        </div>
+
+        <div className="left-column">
+          <a href="#" onClick={handleGuest} className="link-option">Continue as Guest</a>
+          <a href="#" onClick={handleLogin} className="link-option">Login</a>
+        </div>
       </div>
-      <div className="inputs">
-        <input className="input" type="email" placeholder="Email" />
-      </div>
-      <div className="inputs">
-        <input className="input" type="password" placeholder="Password" />
-      </div>
-      <div className="inputs">
-        <input className="input" type="password" placeholder="Confirm Password" />
-      </div>
-      <br />
-      <button type="submit">Submit</button>
-    </form>
-  </div>
-
-  {/* OR Separator */}
-  <div className="or-separator">
-    <span>OR</span>
-  </div>
-
-  {/* Right Column */}
-<div className="left-column">
-  <a href="#" onClick={handleGuest} className="link-option">Continue as Guest</a>
-  <a href="#" onClick={handleLogin} className="link-option">Login</a>
-</div>
-
-</div>
-
     </div>
   );
 };
